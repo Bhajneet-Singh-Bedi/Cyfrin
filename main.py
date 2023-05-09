0b686c5b1c317c115c5740503951f6185faf3237
Bhajneet-Singh-Bedi <bhajneetbedi2002@gmail.com>
2023-05-09 22:40:27 +0530

    Added description

---
+++ main.py
@@ -0,0 +1,20 @@
+# Cyfrin 2023
+# 09-05-2023
+# Bhajneet Singh Bedi
+
+
+
+"""
+This is a password manager app, now this app will store the  passwords and then it will send the password to 
+the drive.
+
+The program will consists of tkinter window which will contain a GUI which will first ask the user to fill some information 
+like user, google drive mail and so on.
+
+Then the program will be such that if the person will ever open the program again it will remember those previously 
+prompted messages and do the rest.
+
+"""
+
+
+ 
