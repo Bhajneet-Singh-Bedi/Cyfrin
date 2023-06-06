0aac6a7feef248e794bc978af08c170cd42b841d
Bhajneet-Singh-Bedi <bhajneetbedi2002@gmail.com>
2023-06-06 15:54:21 +0530

    Username and password checker and error raiser

--- main.py
+++ main.py
@@ -12,21 +12,20 @@ uploaded on the drive linked, else it will be saved locally.
 
 from tkinter import *
 import tkinter as tk
+import tkinter.messagebox
 
-btnn="False"
 ctr=0
 
 def main():
     # Making every content global so that it can be accessed later on in every function.
-    global btn, userName, userNameEntry, userPass, userPassEntry, ctr, addps, getPs
+    global btn, userName, userNameEntry, userPass, userPassEntry, ctr, addps, getPs, btn1
     window = Tk()
     window.geometry("600x600")
     frame=Frame(window)
     frame.place(relx=0.5)
     window.title("Cyfrin")
     
-    # userName = Label(window, text="User Name").grid(row=0, column=0)
-    # userEntry = Entry(window, bd=5).grid(row=0, column=1)
+    # This is the page 1.
     userName = Label(window, text="User Name")
     userName.place(relx=.4, rely=.4,anchor= CENTER)
     userNameEntry = Entry(window, bd=5)
@@ -35,36 +34,41 @@ def main():
 
     userPass = Label(window, text="Password")
     userPass.place(relx=.4, rely=.5,anchor= CENTER)
-    
     userPassEntry = Entry(window, bd=5)
+    
     userPassEntry.place(relx=.6, rely=.5,anchor= CENTER)
     
-    btn = Button(window,
-             text ="Submit",
-             command = Page1)
+    btn = Button(window,text ="Submit",command = Page1)
     btn.place(relx=.5, rely=.6,anchor= CENTER)
-    
+    btn1 = Button(window, text='Back', command=back2one)
     # This is the page 2 content.
     addps = Button(window, text="Add new password", command=addPass)
     getPs = Button(window, text="See Saved Password", command = getPass)
-
+    btn1 = Button(window, text='Back', command=back2one)
 
 
     window.mainloop()
 
 
 def Page1():
-    # Destroying first screen objects.
-    userName.destroy()
-    userNameEntry.destroy()
-    userPass.destroy()
-    userPassEntry.destroy()
-    btn.destroy()
+    # Getting username and pass.
+    usNaEn = userNameEntry.get()
+    usPaEn = userPassEntry.get()
 
-    # Displaying second screen objects.
-    addps.place(relx=.5, rely=.4,anchor= CENTER)
-    getPs.place(relx=.5, rely=.5,anchor= CENTER)
+    # checking username and pass
+    if (usNaEn == 'bhajneet' and usPaEn == '123456'):
+        # Destroying first screen objects.
+        userName.destroy()
+        userNameEntry.destroy()
+        userPass.destroy()
+        userPassEntry.destroy()
+        btn.destroy()
 
+        # Displaying second screen objects.
+        addps.place(relx=.5, rely=.4,anchor= CENTER)
+        getPs.place(relx=.5, rely=.5,anchor= CENTER)
+    else:
+        tk.messagebox.showinfo("Error", "The username or password is incorrect!!")
 
 def getPass():
     pass
@@ -72,6 +76,17 @@ def getPass():
 
 def addPass():
     pass
+
+
+def back2one():
+    """"
+    addps.destroy()
+    getPs.destroy()
+    btn1.destroy()
+    """
+    pass
+
+
 
 if __name__=="__main__":
     main()
