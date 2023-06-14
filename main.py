6ffbfc9b570b1116c598d9bf92fc77942e304b19
Bhajneet-Singh-Bedi <bhajneetbedi2002@gmail.com>
2023-06-14 12:36:13 +0530

    add password complete

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 def main():
     # Making every content global so that it can be accessed later on in every function.
-    global window, btn, userName, userNameEntry, userPass, userPassEntry, addps, getPs, addName, addNameEntry, addPassword, addPassEntry, secretPass, secretPassEntry, submitAdd
+    global window, btn, userName, userNameEntry, userPass, userPassEntry, addps, getPs, addName, addNameEntry, addPassword, addPassEntry, secretPass, secretPassEntry, submitAdd, secPass, secPassEntry
     window = Tk()
     window.geometry("600x600")
     frame=Frame(window)
@@ -48,7 +48,7 @@ def main():
     btn.place(relx=.5, rely=.6,anchor= CENTER)
     # This is the page 2 content.
     addps = Button(window, text="Add new password", command=addPass)
-    getPs = Button(window, text="See Saved Password", command = getPass)
+    # getPs = Button(window, text="See Saved Password", command = getPass)
 
     ######### Add pass content #########
     addName = Label(window, text="Add web link")
@@ -59,7 +59,11 @@ def main():
     secretPassEntry = Entry(window, bd=5)
     submitAdd = Button(window, text='Add Password', command=addpsButton)
 
-    
+    ######### Get Pass content #########
+    # secPass = Label(window, text='Enter secret Password')
+    # secPassEntry = Entry(window, bd=5)
+
+
     # print(os.getcwd())
     # os.chdir('C:')
     # print(os.getcwd())
@@ -91,7 +95,7 @@ def Page1():
     if not os.path.isfile(usNaEn+'.csv'):
         f = open(usNaEn+'.csv','w')
         writer=csv.writer(f)
-        writer.writerow(['Web Link', 'Password'])
+        writer.writerow(['Web Link', 'Password', 'Secret Password'])
         f.close()
 
     
@@ -106,7 +110,7 @@ def Page1():
 
         # Displaying second screen objects.
         addps.place(relx=.5, rely=.4,anchor= CENTER)
-        getPs.place(relx=.5, rely=.5,anchor= CENTER)
+        # getPs.place(relx=.5, rely=.5,anchor= CENTER)
     else:
         tk.messagebox.showinfo("Error", "The username or password is incorrect!!")
 
@@ -124,7 +128,7 @@ def addPass():
     # This function includes add pass functionality.
     # delete all content
     addps.destroy()
-    getPs.destroy()
+    # getPs.destroy()
     # Add the website link
     addName.place(relx=.408, rely=.4,anchor= CENTER)
     addNameEntry.place(relx=.6, rely=.4,anchor= CENTER)
@@ -149,15 +153,21 @@ def addpsButton():
     webPassword = addPassEntry.get()
     secPass = secretPassEntry.get()
 
+    # f = open(usNaEn+'.csv','w')
+    # writer=csv.writer(f)
+    # writer.writerow(['Web Link', 'Password'])
+    # f.close()
+
     
     encWebPassword = encrypt(webPassword, secPass)
-    
+    print(type(encWebPassword))
+    encSecPassword = encrypt(secPass, secPass)
     # Now we have the password and file now let's save it.
     # os.chdir('Cyfrin')
     # print(os.getcwd())
     with open(usNaEn+'.csv','a') as file:
         writer = csv.writer(file)
-        writer.writerow([webLink, encWebPassword])
+        writer.writerow([webLink, encWebPassword, encSecPassword])
     # print(encWebLink,"\t",encWebPassword)
     
     # print(decrypt(encWebLink,secPass),"\t",decrypt(encWebPassword,secPass))
@@ -207,14 +217,28 @@ def decrypt(cipher_text, password):
 def quitWindow():
     window.destroy()
 
-def getPass():
-    with open(usNaEn+'.csv','r') as file:
-        reader = csv.DictReader(file)
-        values=[]
-        for row in reader:
-            value = row['Password']
-            values.append(value)
+# def getPass():
+#     addps.destroy()
+#     getPs.destroy()
+#     secPass.place(relx=0.4, rely=0.5, anchor=CENTER)
+#     secPassEntry.place(relx=0.65, rely=0.5, anchor=CENTER)
+#     secGetPass = secPassEntry.get()
 
+#     with open(usNaEn+'.csv','r') as file:
+#         reader = csv.DictReader(file)
+#         pss = []
+#         for row in reader:
+#             pss.append(row['Secret Password'])
+#         # print(bytes(pss[0], 'utf-8'))
+#         # print(type(bytes(pss[0], 'utf-8')))
+#     if secGetPass == decrypt(pss[0], secGetPass):
+#         with open(usNaEn+'.csv','r') as file:
+#             reader = csv.DictReader(file)
+#             values=[]
+#             for row in reader:
+#                 value = row['Password']
+#                 values.append(value)
+    
 
 
 if __name__=="__main__":
